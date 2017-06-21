461d0cc52d8f3a5070937f54b014ee06a1ca13f4
nurapip41 <nurapip@41studio.com>
2017-06-21 13:24:13 +0700

    first commit

---
+++ db/migrate/20170621062153_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :description
+      t.decimal :price
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
